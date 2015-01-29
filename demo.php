81d03c102fe0a86c64910d68f072b2c4f8640fab
Wei Fang <william_w_fang@yahoo.com>
2015-01-28 21:26:23 -0500

    remove demo.php

--- demo.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-require 'class.Address.inc.php';
-
-echo '<h2>Instaniating Address</h2>';
-$address = new Address;
-
-echo '<h2>Empty Address</h2>';
-echo '<tt><pre>'. var_export($address, TRUE).'</pre>';
